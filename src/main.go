def6e22fdca8ebeb1a21649279936bb51a5898bb
hallsamir14 <hallsamir14@gmail.com>
2025-04-28 22:01:38 -0400

    print deserialized data after successful parse

--- src/main.go
+++ src/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"golangDockerDemo/parser"
 )
 
 func main() {
 	json_data := parser.Json_payload{}
-	message, err := json_data.Get_file("complex.json")
+	message, err := json_data.Read_from_file("complex.json")
 	json_data.Parse_json(message)
 
 	if err != nil {
 		panic(err)
 	}
+
+	fmt.Println(message)
+
 }
